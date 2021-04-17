9f271b8e5a6cdc38ff630de9805cd2e516b7ed66
Spencer Henry <spencerhenry94@gmail.com>
2021-04-17 19:35:41 -0400

    added more question prompts

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'project title',
             message: 'What is the name of the project?'
+        },
+        {
+            type: 'input',
+            name: 'author name',
+            message: 'Who is the author?'
+        },  
+        {
+            type: 'input',
+            name: 'project description',
+            message: 'Provide a description of the project (Required):'
+        },
+        {
+            type:'checkbox',
+            name: 'languages',
+            message: 'Which languages was the project built with?',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'installation steps',
+            message: 'What are the steps neccessary to install the application?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use.'
         }
-    ])
-.then(answers => console.log(answers));
+    ]);
+};    
+
+promptUser().then(answers => console.log(answers));
 
 
 /*const fs = require('fs');
